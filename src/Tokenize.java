4758379a5ed430c7aaf6810b213e66cf355c1bde
Abhishek Bhowmick <abhowmick22@gmail.com>
2015-03-20 17:50:30 -0400

    Add tokenizer for input

---
+++ src/Tokenize.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+/*
+ * This program reads in a file line by line, tokenizes
+ * it and outputs the tokens in a stream (new-line delimited).
+ */
+
+public class Tokenize {
+	
+	public static void main(String[] args){
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String line = null;
+	
+		try {
+			while((line = br.readLine()) != null){			// for all lines in the input stream
+				String[] words = line.split("\\s+");
+				String result = "";
+				for (String word : words){
+					word = word.replaceAll("\\W", "");		// remove non-word characters
+					result += word + "\n";					// delimit by new-line
+				}
+			
+				bw.write(result);
+				bw.flush();
+			
+			}
+		} catch (IOException e) {
+			System.out.println("Trouble reading input or writing output.");
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/WordCount.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+/* 
+ * This program counts all the words from the input stream
+ * and outputs the counts to standard output. The 
+ * Input : A stream of tokens consisting of 1 word per line, 
+ * sorted lexicographically.
+ * Output : A stream of <word, count> pairs, for each word seen
+ * in the input.
+ */
+public class WordCount {
+	
+	public static void main(String[] args){
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+	}
+	
+
+}
